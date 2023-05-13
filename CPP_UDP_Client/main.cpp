93b8a3342f17c29d58bfa5eabc4e54fa54b991c6
Chip Brommer <chipbrommer@gmail.com>
2023-05-13 12:34:20 -0500

    Unicast send test = good.

--- CPP_UDP_Client/main.cpp
+++ CPP_UDP_Client/main.cpp
@@ -4,8 +4,9 @@
 #include <iostream>
 #include "Source/udp_client.h"
 
-//#define BROADCAST_TEST
-#define MULTICAST_TEST
+#define UNICAST_SEND_TEST
+//#define BROADCAST_SEND_TEST
+//#define MULTICAST_SEND_TEST
 
 int main()
 {
@@ -13,7 +14,7 @@ int main()
 	Essentials::Communications::UDP_Client* udp = new Essentials::Communications::UDP_Client();
 	std::cout << Essentials::Communications::UdpClientVersion;
 
-#ifdef BROADCAST_TEST
+#ifdef BROADCAST_SEND_TEST
 	if (udp->EnableBroadcast(8080) < 0)
 	{
 		std::cout << udp->GetLastError();
@@ -21,8 +22,25 @@ int main()
 	std::string buffer = "Hello World!";
 	std::string buffer2 = "Hello From Broadcasting!";
 
-#elif defined UNICAST_TEST
-#elif defined MULTICAST_TEST
+#elif defined UNICAST_SEND_TEST
+	if (udp->ConfigureThisClient("127.0.0.2", 5002) < 0)
+	{
+		std::cout << udp->GetLastError();
+	}
+
+	if (udp->SetDestination("127.0.0.1", 5001) < 0)
+	{
+		std::cout << udp->GetLastError();
+	}
+
+	if (udp->Open() < 0)
+	{
+		std::cout << udp->GetLastError();
+	}
+
+	std::string buffer = "Hello Server!";
+	std::string buffer2 = "Hello From Client!";
+#elif defined MULTICAST_SEND_TEST
 	if (udp->EnableMulticast("239.255.0.1", 8888) < 0)
 	{
 		std::cout << udp->GetLastError();
@@ -35,13 +53,13 @@ int main()
 
 	std::string buffer = "Hello Group!";
 	std::string buffer2 = "Hello From Multicasting!";
-#endif // BROADCAST_TEST
+#endif // TESTS
 
 	int sendcount = 0;
 
 	for (;;)
 	{
-#ifdef BROADCAST_TEST
+#ifdef BROADCAST_SEND_TEST
 		if (udp->Send(buffer.c_str(), buffer.length(), Essentials::Communications::SendType::BROADCAST) < 1)
 		{
 			std::cout << "FAIL: " << udp->GetLastError() << std::endl;
@@ -55,8 +73,21 @@ int main()
 		{
 			sendcount++;
 		}
-#elif defined UNICAST_TEST
-#elif defined MULTICAST_TEST
+#elif defined UNICAST_SEND_TEST
+		if (udp->Send(buffer.c_str(), buffer.length(), Essentials::Communications::SendType::UNICAST) < 1)
+		{
+			std::cout << "FAIL: " << udp->GetLastError() << std::endl;
+		}
+
+		if (udp->SendUnicast(buffer2.c_str(), buffer2.length()) < 1)
+		{
+			std::cout << "FAIL 2: " << udp->GetLastError() << std::endl;
+		}
+		else
+		{
+			sendcount++;
+		}
+#elif defined MULTICAST_SEND_TEST
 		if (udp->Send(buffer.c_str(), buffer.length(), Essentials::Communications::SendType::MULTICAST) < 1)
 		{
 			std::cout << "FAIL: " << udp->GetLastError() << std::endl;
@@ -70,8 +101,7 @@ int main()
 		{
 			sendcount++;
 		}
-#endif // BROADCAST_TEST
-
+#endif // TESTS
 	}
 
 	return 0;
