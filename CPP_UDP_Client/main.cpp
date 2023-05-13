bb0df85f334102449781fa8c35a11316515736ae
Chip Brommer <chipbrommer@gmail.com>
2023-05-13 12:00:28 -0500

    Multicast test: send = pass

--- CPP_UDP_Client/main.cpp
+++ CPP_UDP_Client/main.cpp
@@ -4,19 +4,27 @@
 #include <iostream>
 #include "Source/udp_client.h"
 
-#define BROADCAST_TEST
+//#define BROADCAST_TEST
+#define MULTICAST_TEST
 
 int main()
 {
 	std::cout << "Hello CMake." << std::endl;
 	Essentials::Communications::UDP_Client* udp = new Essentials::Communications::UDP_Client();
 	std::cout << Essentials::Communications::UdpClientVersion;
-	//udp->Configure("127.0.0.1",8080);
-	//udp->Open();
-	//std::cout << udp->GetLastError();
-	udp->EnableBroadcast(8080);
-	std::cout << udp->GetLastError();
 
+#ifdef BROADCAST_TEST
+	if (udp->EnableBroadcast(8080) < 0)
+	{
+		std::cout << udp->GetLastError();
+	}
+#elif defined UNICAST_TEST
+#elif defined MULTICAST_TEST
+	if (udp->EnableMulticast("239.255.0.1", 8888) < 0)
+	{
+		std::cout << udp->GetLastError();
+	}
+#endif // BROADCAST_TEST
 	std::string buffer = "Hello Receiver!";
 	std::string buffer2 = "Hello Again!";
 
@@ -33,15 +41,6 @@ int main()
 
 	for (;;)
 	{
-		//if (udp->Send((char*)&MSG, sizeof(msg)) < 1)
-		//{
-		//	std::cout << "FAIL: " << udp->GetLastError() << std::endl;
-		//}
-		//else
-		//{
-		//	sendcount++;
-		//}
-
 #ifdef BROADCAST_TEST
 		if (udp->Send(buffer.c_str(), buffer.length(), Essentials::Communications::SendType::BROADCAST) < 1)
 		{
@@ -58,6 +57,19 @@ int main()
 		}
 #elif defined UNICAST_TEST
 #elif defined MULTICAST_TEST
+		if (udp->Send(buffer.c_str(), buffer.length(), Essentials::Communications::SendType::MULTICAST) < 1)
+		{
+			std::cout << "FAIL: " << udp->GetLastError() << std::endl;
+		}
+
+		if (udp->SendMulticast(buffer2.c_str(), buffer2.length()) < 1)
+		{
+			std::cout << "FAIL 2: " << udp->GetLastError() << std::endl;
+		}
+		else
+		{
+			sendcount++;
+		}
 #endif // BROADCAST_TEST
 
 	}
